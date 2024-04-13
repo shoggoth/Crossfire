33c427a6987fdaa1ff6bf01ad675b96f4ce1f359
rich@wight <r.j.henry@gmail.com>
2024-04-13 11:02:04 +0100

    Working on selection menu

--- scene/ui.gd
+++ scene/ui.gd
@@ -13,3 +13,24 @@ func _on_button_pressed():
 func _set_mode(value: String):
 	mode = value
 	get_children().map(func(child): if child.name == mode: child.show() else: child.hide())
+
+
+
+class SelectionMenu extends Control:
+	
+	@export var ui: UI
+	
+	var selection_items: Array[Node]
+	var selection_index: int:
+		set(value):
+			selection_index = clamp(value, 0, selection_items.size() - 1)
+			selection_items.map(func(node): node.modulate.a = 0.3)
+	
+	
+	func _ready():
+		selection_items = get_children()
+		selection_index = 0
+	
+	
+	func _input(event):
+		print(event)
