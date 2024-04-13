288a0db6248492dd9f59d8dd7cad98e1fb610cb6
rich@wight <r.j.henry@gmail.com>
2024-04-13 16:53:53 +0100

    Can tab through buttons

--- scene/ui.gd
+++ scene/ui.gd
@@ -6,31 +6,14 @@ signal start_game
 var mode: String: set = _set_mode
 
 
-func _on_button_pressed():
-	start_game.emit()
-
-
 func _set_mode(value: String):
 	mode = value
 	get_children().map(func(child): if child.name == mode: child.show() else: child.hide())
 
 
+func _on_new_game_pressed():
+	start_game.emit()
 
-class SelectionMenu extends Control:
-	
-	@export var ui: UI
-	
-	var selection_items: Array[Node]
-	var selection_index: int:
-		set(value):
-			selection_index = clamp(value, 0, selection_items.size() - 1)
-			selection_items.map(func(node): node.modulate.a = 0.3)
-	
-	
-	func _ready():
-		selection_items = get_children()
-		selection_index = 0
-	
-	
-	func _input(event):
-		print(event)
+
+func _on_exit_pressed():
+	get_tree().quit()
