ace818cfef14b06f52daf2689ae12806788c58a3
rich@wight <r.j.henry@gmail.com>
2024-04-20 15:40:33 +0100

    Move player to the World node

--- scene/game.gd
+++ scene/game.gd
@@ -14,13 +14,11 @@ func _ready():
 func _on_game_start():
 	remove_child(attract)
 	world = $World.create_instance()
-	player = $Player.create_instance()
 	$UI.mode = "HUD"
 
 
 #TODO: Remove the player param
 func _on_game_over(_player):
 	remove_child(world)
-	remove_child(player)
 	if !attract.is_inside_tree(): attract = $Attract.create_instance()
 	$UI.mode = "StartMenu"
