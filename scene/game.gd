ce7191dbef26da2f41056838a300f6b292fb4e88
rich@wight <r.j.henry@gmail.com>
2024-04-22 15:35:49 +0100

    Create initial fade transition

--- scene/game.gd
+++ scene/game.gd
@@ -5,16 +5,23 @@ var world: Node = null
 var attract: Node = null
 var player: Node = null
 
+@onready var transition = $Transition
+
 
 func _ready():
 	attract = $Attract.create_instance()
 	$UI.mode = "StartMenu"
+	transition.fade(false)
 
 
 func _on_game_start():
+	transition.fade(true)
+	await transition.faded_out
 	remove_child(attract)
-	world = $World.create_instance()
 	$UI.mode = "HUD"
+	transition.fade(false)
+	world = $World.create_instance()
+	await transition.faded_in
 
 
 #TODO: Remove the player param
