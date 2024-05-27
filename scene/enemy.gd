e608d870922d6ea03f484e31e7d9aeb495bb8675
rich@wight <r.j.henry@gmail.com>
2024-05-27 13:21:13 +0200

    Enemies hide and lurk movement

--- scene/enemy.gd
+++ scene/enemy.gd
@@ -10,6 +10,7 @@ var destroyed: Callable
 func _ready():
 	$Sprite2D.frame = type
 	$StateMachine.set_meta("move_component", $MoveComponent)
+	$StateMachine/Lurk.home_pos = 0.5 * (Vector2.RIGHT if spawn_pos.y == 0 else Vector2.DOWN)
 
 
 func _on_health_component_health_changed(health):
--- src/global.gd
+++ src/global.gd
@@ -6,4 +6,4 @@ var game: Node
 
 func quantise(vec: Vector2) -> Vector2:
 	if vec.x && vec.y: vec.x = 0
-	return vec.normalized()
+	return vec
