31b0277175f1a7819dbab26534171a1a65db03f6
rich@wight <r.j.henry@gmail.com>
2024-06-03 16:10:17 +0200

    Stop hiding initially

--- scene/enemy.gd
+++ scene/enemy.gd
@@ -11,8 +11,8 @@ func _ready():
 	$Sprite2D.frame = type
 	$StateMachine.set_meta("move_component", $MoveComponent)
 	var hp = 0.5 * (Vector2.RIGHT if spawn_pos.y == 0 else Vector2.DOWN)
-	$StateMachine/Hide.home_pos = hp
-	$StateMachine/Lurk.home_pos = -hp
+	$StateMachine/Lurk.home_pos = hp
+	$StateMachine/Hide.home_pos = -hp
 
 
 func _on_health_component_health_changed(health):
--- scene/world.gd
+++ scene/world.gd
@@ -8,12 +8,12 @@ func _ready():
 	$EnemySpawn.snapshot.player = $Player		# TODO: Handle player respawn
 
 
-func _on_player_destroyed(player: Player):
-	player.queue_free()
+func _on_player_destroyed(p: Player):
+	p.queue_free()
 
 
-func _on_enemy_destroyed(enemy):
-	enemy.queue_free()
+func _on_enemy_destroyed(e: Enemy):
+	e.queue_free()
 
 
 func _on_enemy_spawn_snapshot_changed(snapshot):
