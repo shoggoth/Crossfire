fabed96c8b4a419d05da12c1257a2c279eaea4b1
rich@wight <r.j.henry@gmail.com>
2024-07-01 15:17:07 +0200

    Added small font bitmap

--- src/util/text_node.gd
+++ src/util/text_node.gd
@@ -2,7 +2,7 @@ class_name TextNode extends Node2D
 
 
 var text: String
-var _default_font: Font = ThemeDB.fallback_font
+var _default_font: Font = ThemeDB.get_default_theme().get_font("SmallFont", "")
 
 func _draw():
-	draw_string(_default_font, Vector2(0, 0), text, HORIZONTAL_ALIGNMENT_CENTER)
+	draw_string(_default_font, Vector2(0, 0), text, HORIZONTAL_ALIGNMENT_CENTER, -1, 4)
