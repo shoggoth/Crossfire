6524f5095586b11bf75b30d33ac037d41e197347
rich@wight <r.j.henry@gmail.com>
2024-07-11 15:22:54 +0100

    Added mix bus and FX channel

--- src/util/text_node.gd
+++ src/util/text_node.gd
@@ -5,4 +5,4 @@ var text: String
 var _default_font: Font = ThemeDB.get_default_theme().get_font("SmallFont", "")
 
 func _draw():
-	draw_string(_default_font, Vector2(0, 0), text, HORIZONTAL_ALIGNMENT_CENTER, -1, 4)
+	draw_string(_default_font, Vector2(0, 0), text, HORIZONTAL_ALIGNMENT_CENTER, -1, 5)
