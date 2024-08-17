e6a101e825bd572a547d97039fecd0569f225dca
rich@wight <r.j.henry@gmail.com>
2024-08-17 15:36:15 +0100

    Update to 4.3

--- src/component/ai.gd
+++ src/component/ai.gd
@@ -17,8 +17,7 @@ var _thinking_time = 0.0
 
 
 func _physics_process(_delta):
-	if ray_cast.is_colliding():
-		player_direction = enemy.position.direction_to(ray_cast.get_collision_point())
+	player_direction = enemy.position.direction_to(ray_cast.get_collision_point()) if ray_cast.is_colliding() else Vector2.ZERO
 
 
 func _process(delta):
@@ -43,7 +42,7 @@ func _on_move_component_finished_moving(_mc):
 		#"Track": state_machine.enter_state_named("Track", { "destination" : Vector2(randi_range(1, 6), randi_range(1, 5)) })
 		"Track":
 			if player_direction:
-				state_machine.enter_state_named("Fire", { "direction" : player_direction })
+				state_machine.enter_state_named("Fire", { "direction" : player_direction.normalized() })
 			else:
 				ray_cast.rotation += PI * 0.5
 				state_machine.enter_state_named("Track", { "destination" : Vector2(randi_range(1, 6), randi_range(1, 5)) })
