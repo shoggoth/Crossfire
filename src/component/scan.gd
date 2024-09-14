8fe388999c2b72fb6074cd598bea18b80295af93
rich@wight <r.j.henry@gmail.com>
2024-09-14 14:51:54 +0200

    Add threat detect signal (not connected yet)

--- src/component/scan.gd
+++ src/component/scan.gd
@@ -1,6 +1,8 @@
 class_name ScanComponent extends Node2D
 
 
+signal threat_detected(direction: Vector2)
+
 @export var scanning := false:
 	set(value):
 		scanning = value
@@ -18,12 +20,10 @@ func _ready() -> void:
 	scanning = false
 
 
-func _physics_process(_delta):
-	var player_direction = Vector2.RIGHT.rotated(player_ray_detect.rotation) if player_ray_detect.is_colliding() else Vector2.ZERO
-
-
 func _on_timer_timeout() -> void:
-	if scanning: player_ray_detect.rotation += scan_increment
+	if scanning:
+		if player_ray_detect.is_colliding(): threat_detected.emit(Vector2.RIGHT.rotated(player_ray_detect.rotation))
+		else: player_ray_detect.rotation += scan_increment
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
