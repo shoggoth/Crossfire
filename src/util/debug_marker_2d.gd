0f084e7bdccfc2bbf30a24ad198deb024b762fbf
rich@wight <r.j.henry@gmail.com>
2025-06-13 13:46:26 +0100

    Improve debug marker drawing

--- src/util/debug_marker_2d.gd
+++ src/util/debug_marker_2d.gd
@@ -4,19 +4,21 @@ class_name DebugMarker2D extends Marker2D
 @export var line_width = -1.0
 @export var darken = 0.5
 @export var centre_colour = Color.WHITE
+@export var axis_length = 10.0
 
 var _x_colour = Color.RED
 var _y_colour = Color.GREEN
 
 func _draw() -> void:
-	var x_off = Vector2(gizmo_extents, 0)
-	var y_off = Vector2(0, gizmo_extents)
+	var x_off = Vector2(axis_length, 0)
+	var y_off = Vector2(0, axis_length)
 	
 	# Draw axes
 	draw_line(origin, origin + x_off, _x_colour, line_width)
 	draw_line(origin, origin + y_off, _y_colour, line_width)
-	draw_line(origin, origin - x_off, _x_colour.darkened(darken), line_width)
-	draw_line(origin, origin - y_off, _y_colour.darkened(darken), line_width)
+	var df = clamp(darken, 0, 1)
+	draw_line(origin, origin - x_off, _x_colour.darkened(df), line_width)
+	draw_line(origin, origin - y_off, _y_colour.darkened(df), line_width)
 	
 	# Draw centre
 	var lw = 1.0 if line_width < 0 else line_width
